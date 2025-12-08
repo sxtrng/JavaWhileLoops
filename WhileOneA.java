cade51e045a5ef3c3965ed462e47fb050a706b89
sxtrng <sxtrng@gmail.com>
2025-12-08 08:00:18 -0500

    While Loop - Counting Down

---
+++ WhileOneA.java
@@ -0,0 +1,14 @@
+public class WhileOneA {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        while (x > 0) {
+            System.out.print(x + " ");
+            x--;
+        }
+
+        System.out.println("\n");
+
+    }
+}
