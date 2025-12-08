6d3fc8d4de2dface9b1fca0ea9d8033c1de827ed
sxtrng <sxtrng@gmail.com>
2025-12-08 07:57:46 -0500

    Basic While Loop.

---
+++ WhileOne.java
@@ -0,0 +1,12 @@
+public class WhileOne {
+    public static void main(String[] args) {
+
+        int x = 0;
+
+        while (x < 10) {
+            System.out.print(x + " ");
+            x++;
+        }
+
+    }
+}
