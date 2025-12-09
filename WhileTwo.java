4ec9babef7a5adadd04712c38d021f25246d106c
sxtrng <sxtrng@gmail.com>
2025-12-09 06:49:06 -0500

    Incrementing by 2.

---
+++ WhileTwo.java
@@ -0,0 +1,14 @@
+public class WhileTwo {
+    public static void main(String[] args) {
+
+        int x = 0;
+
+        while (x <= 20) {
+            System.out.print(x + " ");
+            x += 2;
+        }
+
+        System.out.println("\n");
+
+    }
+}
